b1e7cf274156bc1bfa798a955ae75a95214889e8
Simon Pirko <sipi7gm@gmail.com>
2026-04-16 14:28:49 +0300

    Initial commit

---
+++ src/main/java/by/tms/twitterapiprojectc38onl/TwitterApiProjectC38OnlApplication.java
@@ -0,0 +1,13 @@
+package by.tms.twitterapiprojectc38onl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApiProjectC38OnlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterApiProjectC38OnlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/tms/twitterapiprojectc38onl/TwitterApiProjectC38OnlApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tms.twitterapiprojectc38onl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterApiProjectC38OnlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
