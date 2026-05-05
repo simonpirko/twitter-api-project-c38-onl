a0ea81558757098f74a7b816c8fa6942fa32282c
Dasha2203 <kitkatlolipop@mail.ru>
2026-05-06 00:24:36 +0300

    fix: update api of posts

--- src/main/java/by/tms/twitterapiprojectc38onl/dto/PostCreateDTO.java
+++ src/main/java/by/tms/twitterapiprojectc38onl/dto/PostCreateDTO.java
@@ -1,5 +1,6 @@
 package by.tms.twitterapiprojectc38onl.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.ElementCollection;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -10,13 +11,17 @@ import java.util.List;
 public class PostCreateDTO {
 
     @NotBlank
+    @Schema(example = "My first post", description = "Title of post", requiredMode = Schema.RequiredMode.REQUIRED)
     private String title;
 
     @NotBlank
+    @Schema(example = "Description of my first post", description = "Description of post", requiredMode = Schema.RequiredMode.REQUIRED)
     private String description;
 
     @ElementCollection
+    @Schema(example = "[\"url1\", \"url2\"]", description = "The array of links to imgs", requiredMode = Schema.RequiredMode.REQUIRED)
     private List<String> imageUrls;
 
+    @Schema(example = "1", description = "The id of channel, where post will be created", requiredMode = Schema.RequiredMode.REQUIRED)
     private Long channelId;
 }
