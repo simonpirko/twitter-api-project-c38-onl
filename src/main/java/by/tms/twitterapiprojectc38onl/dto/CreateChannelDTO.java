8f31b7e96f21f44a7c281e1c9a34a3aa95e11388
Dasha2203 <kitkatlolipop@mail.ru>
2026-05-06 00:24:09 +0300

    fix: update api channel

--- src/main/java/by/tms/twitterapiprojectc38onl/dto/CreateChannelDTO.java
+++ src/main/java/by/tms/twitterapiprojectc38onl/dto/CreateChannelDTO.java
@@ -1,20 +1,16 @@
 package by.tms.twitterapiprojectc38onl.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CreateChannelDTO {
-
     @NotBlank(message = "The name of channel cannot be blank!")
+    @Schema(example = "Channel name 1", description = "Name of channel", requiredMode = Schema.RequiredMode.REQUIRED)
     private String channelName;
 
     @NotBlank(message = "The description of channel cannot be blank!")
+    @Schema(example = "Descriptions of 'Channel name 1'", description = "Description of channel", requiredMode = Schema.RequiredMode.REQUIRED)
     private String channelDescription;
-
-    @NotBlank
-    private Long channelOwnerId;
-
-    @NotBlank
-    private Long channelModeratorId;
 }
--- src/main/java/by/tms/twitterapiprojectc38onl/dto/UpdateChannelDTO.java
+++ src/main/java/by/tms/twitterapiprojectc38onl/dto/UpdateChannelDTO.java
@@ -1,17 +1,16 @@
 package by.tms.twitterapiprojectc38onl.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UpdateChannelDTO {
-
     @NotBlank(message = "The name of channel cannot be blank!")
+    @Schema(example = "Channel name 1", description = "Name of channel", requiredMode = Schema.RequiredMode.REQUIRED)
     private String channelName;
 
     @NotBlank(message = "The description of channel cannot be blank!")
+    @Schema(example = "Descriptions of 'Channel name 1'", description = "Description of channel", requiredMode = Schema.RequiredMode.REQUIRED)
     private String channelDescription;
-
-    @NotBlank
-    private Long channelModeratorId;
 }
